13342cbff817a3a51499e08b11d54747465f57fd
Sam Washington <spwashi2@illinois.edu>
2017-01-20 22:48:14 -0600

    added a "clear log" button

--- Factshift/views/dev/index.php
+++ Factshift/views/dev/index.php
@@ -4,39 +4,45 @@
  * Date: 6/13/15
  * Time: 6:28 PM
  */
+use Factshift\Core\Factshift;
+
 ?>
-
 <article id="main" class="module">
-	<header>
-		<h1 class="title">
-			{{title}}
-		</h1>
-
-		<h2 class="subtitle"> {{subtitle}} </h2>
-	</header>
-	<section class="content">
-		<section class="subsection">
-			<header>
-				<h3>This is the title of the section</h3>
-			</header>
-			<p>This is the description of the section</p>
-		</section>
-		<section class="subsection">
-			<header>
-				<h3>This is another section</h3>
-				<h5>A subtitle</h5>
-			</header>
-
-			<p>The content of the second section</p>
-			<section class="subsection">
-				<header>
-					<h3>A section in a section</h3>
-					<h5>Section Subtitle</h5>
-
-				</header>
-
-				<p>Content</p>
-			</section>
-		</section>
-	</section>
+    <header>
+        <h1 class="title">
+            {{title}}
+        </h1>
+        
+        <h2 class="subtitle"> {{subtitle}} </h2>
+    </header>
+    <section class="content">
+        <table>
+            <tr>
+                <td>Clear log file:</td>
+                <td>
+                    <button id="clear-log" class="dev clear log">Clear</button>
+                </td>
+                <td id="success"></td>
+            </tr>
+        </table>
+    </section>
 </article>
+<script type="text/javascript">
+    require(['<?= Factshift::_()->base_url ?>resource/js/alpha/require_config.js'], function () {
+        require(['Promise', 'jquery', 'Sm', 'Sm-init'], function (P, $, Sm, init) {
+            if (!Promise) {P.polyfill();}
+            init();
+            $('#clear-log').on('click', function () {
+                var $el = $(this);
+                $.get(window.location + 'cl', function (res) {
+                    Sm.CONFIG.DEBUG && console.log(res);
+                    var child = $el.closest('tr').children('#success');
+                    Sm.CONFIG.DEBUG && console.log(child);
+                    res && (res === true) ? child.html('<i class="fa fa-check" aria-hidden="true"></i>') : '';
+                })
+            });
+            require(['require', 'select2'], function (require) {});
+            require(['require', 'inflection'], function (require) {});
+        });
+    });
+</script>
